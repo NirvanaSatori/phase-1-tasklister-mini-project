4a250a03bf629de2b82c222e1ddac12c6f682dce
MORRIS <morris24kariuki@gmail.com>
2022-12-04 20:21:47 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.querySelector('form')
+  form.addEventlistener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_todo.value)
+    form.reset()
+
+  })
+
+})
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textcontent = `${todo}  `
+  p.appendChild(btn)
+  document.querySelector("#todo_container").appendChild(p)
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
